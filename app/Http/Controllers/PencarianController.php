fffee12bf6e991dc19dc303929f379841abc411c
unknown <seuhendra12@gmail.com>
2023-10-22 01:11:04 +0700

    Menambahkan pencarian UID KTP

---
+++ app/Http/Controllers/PencarianController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penduduk;
+use Illuminate\Http\Request;
+
+class PencarianController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Penduduk::query();
+
+        $search = $request->input('search');
+        if ($search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('uid', 'LIKE', "%$search%");
+            });
+        }
+
+        $penduduks = $search ? $query->get() : null;
+
+        return view('halaman-pencarian.index', compact('penduduks'));
+    }
+}
