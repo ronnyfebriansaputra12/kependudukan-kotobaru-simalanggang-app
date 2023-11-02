96b5ba1feac7e0f5a528adebd8649c6bd9ccdfe3
Dinniatul <kundinnii29@gmail.com>
2023-11-02 17:07:36 +0700

    tambah isi  dashboard di admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Charts\JenisKelamin;
 use App\Models\Penduduk;
+use App\Models\Pengajuan;
 use ConsoleTVs\Charts\Classes\Chartjs\Chart;
 use Illuminate\Http\Request;
 
@@ -14,6 +15,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        $pengajuan = Pengajuan::with('jenisSurat')->get();
         $penduduk = session('penduduk');
         $admin = session('admin');
         if ($penduduk) {
@@ -21,12 +23,62 @@ class DashboardController extends Controller
         }
         if ($admin) {
             $penduduk = Penduduk::all();
+            $pengajuan = Pengajuan::with('jenisSurat')->get();
             $genderCount = $penduduk->groupBy('jekel')->map->count();
+            $domisiliCount = $penduduk->groupBy('dusun')->map->count();
+            $pengajuanCount = $pengajuan->groupBy('id_jenis_surat')->map->count();
+            // dd($pengajuanCount);
+
             $chart = new Chart('bar', 'chartjs');
             $chart->labels(['Laki-laki', 'Perempuan']); // Label jenis kelamin
-            $chart->dataset('Jenis Kelamin', 'bar', [$genderCount->get('Laki-laki', 0), $genderCount->get('Perempuan', 0)])->backgroundColor(['blue', 'pink']);
-            return view('dashboard', compact('admin', 'chart'));
+            $chart->dataset('Jenis Kelamin', 'bar', [$genderCount->get('Laki-laki', 0), $genderCount->get('Perempuan', 0)])->backgroundColor(['blue', 'red']);
+
+            $domisili = new Chart('pie', 'chartjs'); // Menggunakan Pie Chart untuk domisili
+            $domisili->labels(array_map('ucwords', ['Tabek Panjang', 'Koto Baru', 'Parumpang'])); // Label domisili dengan huruf besar di awal kata
+            $domisili->dataset('Domisili', 'pie', [
+                $domisiliCount->get(strtoupper('Tabek Panjang'), 0),
+                $domisiliCount->get(strtoupper('Koto Baru'), 0),
+                $domisiliCount->get(strtoupper('Parumpung'), 0),
+            ])->backgroundColor(['black', 'green', 'yellow']);
+
+            $pengajuanSurat = new Chart('pie', 'chartjs'); // Menggunakan Pie Chart untuk domisili
+            $jenisSuratLabels = $pengajuan->pluck('jenisSurat.name_surat')->toArray();
+            // dd($jenisSuratLabels);
+            $pengajuanSurat->labels(array_map('ucwords', $jenisSuratLabels));
+
+            $jenisSuratColors = [
+                'Surat Keterangan Tidak Mampu' => 'red',
+                'Surat Keterangan Kematian' => 'orange',
+                'Surat Keterangan Domisili' => 'grey',
+            ];
+
+            // dd($pengajuanSurat->datasets);
+            $pengajuanSurat->dataset('Pengajuan Surat', 'pie', [
+                $pengajuanCount->get(strtoupper(1), 0),
+                $pengajuanCount->get(strtoupper(3), 0),
+                $pengajuanCount->get(strtoupper(2), 0),
+            ])->backgroundColor(array_values($jenisSuratColors));
+
+            $suratTidakMampu = 0;
+            $suratDomisili = 0;
+            $suratKematian = 0;
+
+            // Loop through pengajuan
+            foreach ($pengajuan as $p) {
+                // Cocokkan nama surat menggunakan stripos (case-insensitive)
+                if (stripos($p->jenisSurat->name_surat, 'mampu') !== false) {
+                    $suratTidakMampu++;
+                } elseif (stripos($p->jenisSurat->name_surat, 'domisili') !== false) {
+                    $suratDomisili++;
+                } elseif (stripos($p->jenisSurat->name_surat, 'kematian') !== false) {
+                    $suratKematian++;
+                }
+            }
+
+            $pengajuans = Pengajuan::where('status', '1')->latest()->get();
+            // dd($pengajuan);
         }
+        return view('dashboard', compact('admin', 'chart', 'domisili', 'pengajuanSurat','suratTidakMampu', 'suratDomisili', 'suratKematian', 'pengajuans'));
     }
     // public function showChart()
     // {
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,129 +1,261 @@
 @extends('layouts.master')
 
 @section('title', 'Penduduk')
-@section('header', 'Dahboard')
+@section('header', 'Dahsboard')
 @section('breadcrumb', 'dashboard')
 
 @section('container-fluid')
-<!-- Main content -->
-@if (session()->has('admin'))
-<section class="content">
-  <div class="container-fluid">
-    {!! $chart->container() !!}
-    {!! $chart->script() !!}
-  </div>
-</section>
-@endif
+    <!-- Main content -->
+    @if (session()->has('admin'))
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{ $suratTidakMampu }}</h3>
+                                <p>Surat Keterangan Tidak Mampu</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer"> Lihat Pengajuan<i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>{{ $suratDomisili }}</h3>
+                                <p>Surat Keterangan Domisili</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Lihat Pengajuan <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>{{ $suratKematian }}</h3>
+                                <p>Surat Kematian</p>
+                            </div>
 
-@if (session()->has('penduduk'))
-<section class="content">
-  <div class="container-fluid">
-    <!-- Small boxes (Stat box) -->
-    <div class="row">
-      <div class="col-lg-4 col-6">
-        <!-- small box -->
-        <div class="small-box bg-info">
-          <div class="inner">
-            <h3>Surat Keterangan</h3>
-            <p>Tidak Mampu</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-file-alt"></i>
-          </div>
-          <a href="#" class="small-box-footer">Ajukan Surat <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-4 col-6">
-        <!-- small box -->
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h3>Surat Keterangan</h3>
-            <p>Domisili</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-file-alt"></i>
-          </div>
-          <a href="#" class="small-box-footer">Ajukan Surat <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-4 col-6">
-        <!-- small box -->
-        <div class="small-box bg-warning">
-          <div class="inner">
-            <h3 style="overflow-wrap: break-word;">Surat Kematian</h3>
-            <p>Surat Kematian</p>
-          </div>
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Lihat Pengajuan <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
 
-          <div class="icon">
-            <i class="fa fa-file-alt"></i>
-          </div>
-          <a href="#" class="small-box-footer">Ajukan Surat <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
+                <div class="card ">
+                    <div class="card-header">
+                        <div class="text-center fw-bold">Tabel Pengajuan Penduduk </div>
+                    </div>
+                    <div class="card-body">
+                        <div id="example_wrapper">
+                            
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>NO</th>
+                                        <th>NIK</th>
+                                        <th>Jenis Pengajuan</th>
+                                        <th>Tanggal</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($pengajuans as $value)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $value->nik_penduduk }}</td>
+                                            <td>{{ $value->jenisSurat->name_surat }}</td>
+                                            <td>{{  Carbon\Carbon::parse($value->created_at)->isoFormat('DD MMMM Y') }}
+                                            </td>
+                                            <td>
+                                              <div class="badge bg-success">Disetujui</div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
 
-    </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card ">
+                            <div class="card-header">
+                                <div class="text-center fw-bold">Grafik Penduduk Berdasarkan Jenis Kelamin</div>
+                            </div>
+                            <div class="card-body">
+                                {!! $chart->container() !!}
+                                {!! $chart->script() !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card ">
+                            <div class="card-header">
+                                <div class="text-center fw-bold">Grafik Penduduk Berdasarkan Domisili</div>
+                            </div>
+                            <div class="card-body">
+                                {!! $domisili->container() !!}
+                                {!! $domisili->script() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    @endif
 
-  </div>
-</section>
-@endif
-@if (session()->has('admin'))
-<section class="content" hidden>
-  <div class="container-fluid">
-    <!-- Small boxes (Stat box) -->
-    <div class="row">
-      <div class="col-lg-4 col-6">
-        <!-- small box -->
-        <div class="small-box bg-info">
-          <div class="inner">
-            <h3>Surat Keterangan</h3>
+    @if (session()->has('penduduk'))
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>Surat Keterangan</h3>
+                                <p>Tidak Mampu</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Ajukan Surat <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>Surat Keterangan</h3>
+                                <p>Domisili</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Ajukan Surat <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3 style="overflow-wrap: break-word;">Surat Kematian</h3>
+                                <p>Surat Kematian</p>
+                            </div>
 
-            <p>Tidak Mampu</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-file-alt"></i>
-          </div>
-          <a href="#" class="small-box-footer">Ajukan Surat <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-4 col-6">
-        <!-- small box -->
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h3>Surat Keterangan</h3>
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Ajukan Surat <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
 
-            <p>Domisili</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-file-alt"></i>
-          </div>
-          <a href="#" class="small-box-footer">Ajukan Surat <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-4 col-6">
-        <!-- small box -->
-        <div class="small-box bg-warning">
-          <div class="inner">
-            <h3 style="overflow-wrap: break-word;">Surat Kematian</h3>
-            <p>Surat Kematian</p>
-          </div>
+                </div>
 
-          <div class="icon">
-            <i class="fa fa-file-alt"></i>
-          </div>
-          <a href="#" class="small-box-footer">Ajukan Surat <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
+            </div>
+        </section>
+    @endif
+    @if (session()->has('admin'))
+        <section class="content" hidden>
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>Surat Keterangan</h3>
 
-    </div>
+                                <p>Tidak Mampu</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Ajukan Surat <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>Surat Keterangan</h3>
 
-  </div>
-</section>
-@endif
+                                <p>Domisili</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Ajukan Surat <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3 style="overflow-wrap: break-word;">Surat Kematian</h3>
+                                <p>Surat Kematian</p>
+                            </div>
 
-<!-- /.content -->
+                            <div class="icon">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">Ajukan Surat <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    @endif
+
+    <!-- /.content -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+        $(function() {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
+        });
+    </script>
 
 @endsection
