63a309a0b56fd7c04f47f5102e00507443d0197f
Dinniatul <kundinnii29@gmail.com>
2023-11-04 13:13:44 +0700

    update responsive penduduk

--- resources/views/Penduduk/index.blade.php
+++ resources/views/Penduduk/index.blade.php
@@ -19,86 +19,99 @@
                     <input type="text" name="search" id="liveSearch" class="form-control w-25 float-left"
                         placeholder="Search..." onchange="this.form.submit()">
                 </form>
-                <a href="/penduduk" class=" ml-2 btn btn-secondary">Reset</a>
-                <button type="button" class="btn btn-success float-right" data-bs-toggle="modal"
+                <a href="/penduduk" class=" ml-2 btn btn-secondary" title="reset"> <i class="fas fa-sync-alt"
+                        title="reset"></i></a>
+                <button type="button" class="btn btn-success float-right col-sm-" data-bs-toggle="modal"
                     data-bs-target="#exampleModalExcle">
-                    <i class="fa-solid fas fa-file"></i> Import Excel
+                    <span class="d-none d-sm-inline">
+                        <i class="fa-solid fas fa-file"></i> Import Excel
+                    </span>
+                    <span class="d-sm-none" title="import excel">
+                        <i class="fa-solid fas fa-file" title="Import Excel"></i>
+                    </span>
                 </button>
+
 
             </div>
             <!-- /.card-header -->
             <div class="card-body">
                 <div id="example_wrapper">
-                    <table id="example1" class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>UID</th>
-                                <th>NIK</th>
-                                <th>No KK</th>
-                                <th>Nama</th>
-                                <th>Jenis Kelamin</th>
-                                <th>Dusun</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($penduduks as $value)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    {{-- <td>{{ $value->uid ? ucwords($value->uid) : '-' }}</td> --}}
-                                    <td>
-                                        @if ($value->uid === null)
-                                            <form action="{{ url('/uid-penduduk/' . $value->nik) }}" method="post">
-                                                @method('PATCH')
-                                                @csrf
-                                                <input type="text"
-                                                    class="form-control @error('uid') is-invalid @enderror" name="uid"
-                                                    value="{{ $value->uid }}" onchange="this.form.submit()">
-                                                @error('uid')
-                                                    <div class="invalid-feedback">
-                                                        {{ $message }}
-                                                    </div>
-                                                @enderror
-                                            </form>
-                                        @else()
-                                            {{ $value->uid }}
-                                        @endif
-                                    </td>
-                                    <td>{{ ucwords($value->nik) }}</td>
-                                    <td>{{ ucwords($value->no_kk) }}</td>
-                                    <td>{{ ucwords(strtolower($value->nama)) }}</td>
-                                    <td>{{ ucwords(strtolower($value->jekel)) }}</td>
-                                    <td>{{ ucwords(strtolower($value->alamat)) }}</td>
+                    <div class="table-responsive">
 
-                                    <td>
-                                        <div class="d-flex">
-                                            <a href="{{ url('capture/'. $value->nik) }}" id="btn-camera" class="btn btn-success btn-sm me-1"
-                                                data-id="{{ $value->nik }}"><i class="fa-solid fas fa-camera"></i>
-                                            </a>
-                                            <a href="{{ url('/penduduk/' . $value->nik) }}"
-                                                class="btn btn-info btn-sm btn-detail me-1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                    fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                                    <path
-                                                        d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                                                </svg>
-                                                </q>
-                                                <a href="{{ url('/penduduk/' . $value->nik . '/edit') }}"
-                                                    class="btn btn-warning btn-sm me-1">
-                                                    <i class="fa fa-edit"></i>
-                                                </a>
-                                                <a href="#" id="btn-hapus" class="btn btn-danger btn-sm"
-                                                    data-id="{{ $value->nik }}"><i class="fa-solid fas fa-trash"></i>
-                                                </a>
-                                        </div>
-                                    </td>
+
+                        <table id="example1" class="table table-bordered table-striped ">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>UID</th>
+                                    <th>NIK</th>
+                                    <th>No KK</th>
+                                    <th>Nama</th>
+                                    <th>Jenis Kelamin</th>
+                                    <th>Dusun</th>
+                                    <th>Aksi</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($penduduks as $value)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        {{-- <td>{{ $value->uid ? ucwords($value->uid) : '-' }}</td> --}}
+                                        <td>
+                                            @if ($value->uid === null)
+                                                <form action="{{ url('/uid-penduduk/' . $value->nik) }}" method="post">
+                                                    @method('PATCH')
+                                                    @csrf
+                                                    <input type="text"
+                                                        class="form-control @error('uid') is-invalid @enderror"
+                                                        name="uid" value="{{ $value->uid }}"
+                                                        onchange="this.form.submit()">
+                                                    @error('uid')
+                                                        <div class="invalid-feedback">
+                                                            {{ $message }}
+                                                        </div>
+                                                    @enderror
+                                                </form>
+                                            @else()
+                                                {{ $value->uid }}
+                                            @endif
+                                        </td>
+                                        <td>{{ ucwords($value->nik) }}</td>
+                                        <td>{{ ucwords($value->no_kk) }}</td>
+                                        <td>{{ ucwords(strtolower($value->nama)) }}</td>
+                                        <td>{{ ucwords(strtolower($value->jekel)) }}</td>
+                                        <td>{{ ucwords(strtolower($value->alamat)) }}</td>
+
+                                        <td>
+                                            <div class="d-flex">
+                                                <a href="{{ url('capture/' . $value->nik) }}" id="btn-camera"
+                                                    class="btn btn-success btn-sm me-1" data-id="{{ $value->nik }}"><i
+                                                        class="fa-solid fas fa-camera"></i>
+                                                </a>
+                                                <a href="{{ url('/penduduk/' . $value->nik) }}"
+                                                    class="btn btn-info btn-sm btn-detail me-1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                        <path
+                                                            d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                                                    </svg>
+                                                    </q>
+                                                    <a href="{{ url('/penduduk/' . $value->nik . '/edit') }}"
+                                                        class="btn btn-warning btn-sm me-1">
+                                                        <i class="fa fa-edit"></i>
+                                                    </a>
+                                                    <a href="#" id="btn-hapus" class="btn btn-danger btn-sm"
+                                                        data-id="{{ $value->nik }}"><i class="fa-solid fas fa-trash"></i>
+                                                    </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>  
                 </div>
             </div>
             <!-- /.card-body -->
