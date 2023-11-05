5351abb4c98f46d06c39ee13c6da131e496de379
ronnyfebriansaputra12 <ronnyfebriansaputra04@gmail.com>
2023-11-05 11:16:02 +0700

    read notification

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -17,15 +17,18 @@
                     </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                    <span class="dropdown-item dropdown-header">{{ Auth::user()->unreadNotifications->count() }}
-                        Notifications</span>
+                    <span class="dropdown-item dropdown-header">
+                        {{ Auth::user()->unreadNotifications->count() }} Notifications
+                    </span>
                     @foreach (Auth::user()->unreadNotifications as $notification)
-                        <a href="#" class="dropdown-item">
+                        <div class="dropdown-divider"></div>
+                        <a href="{{ url('mark-as-read/'.$notification->id) }}" class="dropdown-item">
                             {{ $notification->data['nik_penduduk'] }} - {{ $notification->data['id_jenis_surat'] }}
                         </a>
                     @endforeach
                 </div>
             </li>
+
             <li class="nav-item">
                 <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                     <i class="fas fa-expand-arrows-alt"></i>
