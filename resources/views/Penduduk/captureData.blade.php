f5091b9400a710bca5d5222d7e10efe46b0e4891
Seuhendra Setiawan <seuhendra12@gmail.com>
2023-11-16 20:11:04 +0700

    Memperbaiki capture data

--- resources/views/Penduduk/captureData.blade.php
+++ resources/views/Penduduk/captureData.blade.php
@@ -3,32 +3,30 @@
 @section('title', 'Verifikasi Penduduk')
 @section('header', 'Verifikasi Penduduk')
 @section('link')
-    <a href="{{ url('penduduk') }}">Penduduk</a>
+<a href="{{ url('penduduk') }}">Penduduk</a>
 @endsection
 @section('breadcrumb', 'Verifikasi Penduduk')
 
 @section('container-fluid')
-    <div class="container">
-        <div class="row">
-            @foreach ($captures as $item)
-                <div class="col-3">
-                    <div class="card">
-                        <div class="card-body text-center">
-                            <div class="col-md-12">
-                                <p class="fw-bold">{{ ucwords(strtolower(substr($item->penduduk->nama, 0, 15))) }} -
-                                    {{ $item->nik_penduduk }}</p>
-                                <img src="{{ $item->file_gambar }}" alt="Gambar" width="200px">
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+<div class="container">
+  <div class="row">
+    @foreach ($captures as $item)
+    <div class="col-3">
+      <div class="card">
+        <div class="card-body text-center">
+          <div class="col-md-12">
+            <p class="fw-bold">{{ ucwords(strtolower(substr($item->penduduk->nama, 0, 15))) }} -
+              {{ $item->nik_penduduk }}
+            </p>
+            <img src="{{ $item->file_gambar }}" alt="Gambar" width="100%">
+          </div>
         </div>
-        <div class="card-footer clearfix">
-            {{ $captures->links('pagination::bootstrap-5') }}
-        </div>
+      </div>
     </div>
+    @endforeach
+  </div>
+  <div class="card-footer clearfix">
+    {{ $captures->links('pagination::bootstrap-5') }}
+  </div>
+</div>
 @endsection
